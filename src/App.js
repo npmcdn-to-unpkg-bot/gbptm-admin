7653c4080234a1096c1190642952cd3b2c80633b
CSGKCJDBCPQ2K8 <x998acc@gmail.com>
2016-08-12 14:38:49 +0100

    moved sidebar

--- src/App.js
+++ src/App.js
@@ -11,17 +11,17 @@ class App extends Component {
     return (
 		<div id="mainContainer" className="container">
 			<div id="innerContainer">
+				<span className="topbar">
+					<div id="sb_button">
+						<img src={sidebar} className="sidebar-button" alt="logo" />
+					</div>
+					<h1>GBPTM Admin Panel</h1>
+				</span>
 				<div id="sidebar" className="col">
 					<h1>Pages</h1>
 					<div id="sb_flexbox"></div>
 				</div>
 				<div id="main" className="col" >
-					<span className="topbar">
-						<div id="sb_button">
-							<img src={sidebar} className="sidebar-button" alt="logo" />
-						</div>
-						<h1>GBPTM Admin Panel</h1>
-					</span>
 					<TotalDisplay source="http://localhost:3002/statistics"/>
 				</div>
 			</div>
